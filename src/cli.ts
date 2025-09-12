1db34f29df1bcce218ae1fac7e60ea6aeb447004
Chris Green <chrisgreen1993@gmail.com>
2025-09-12 17:50:06 +0700

    Use yargs for argument parsing so we get nice help text

--- src/cli.ts
+++ src/cli.ts
@@ -2,38 +2,54 @@
 
 import chalk from "chalk";
 import columns from "cli-columns";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import { allVersions } from "./index";
 import type { PackageVersionInfo } from "./types";
 
-const USAGE_TEXT = `
-Usage: whatver [package name] [semver range]
-
-Example: whatver lodash "^1.1"
-`;
-
-function parseArguments(args: string[]): string[] {
-	const [, , ...programArgs] = args;
-	return programArgs;
-}
-
 function colourValidVersions(versions: PackageVersionInfo[]): string[] {
 	return versions.map(({ version, satisfied }) =>
-		satisfied ? chalk.green(version) : version,
+		satisfied ? chalk.green.bold(version) : version,
 	);
 }
 
-(async (args: string[]) => {
-	try {
-		const programArgs = parseArguments(args);
-		const [pkgName, semverRange] = programArgs;
-		if (!programArgs.length || programArgs[0] === "--help" || !pkgName) {
-			console.log(USAGE_TEXT);
-			return;
-		}
-		const versions = await allVersions(pkgName, semverRange);
-		const versionsWithColour = colourValidVersions(versions);
-		console.log(columns(versionsWithColour, { sort: false }));
-	} catch (error) {
-		console.error(error instanceof Error ? error.message : error);
-	}
-})(process.argv);
+yargs(hideBin(process.argv))
+	.command(
+		"$0 <package> [range]",
+		"Check npm package versions against semver ranges",
+		(yargs) => {
+			return yargs
+				.positional("package", {
+					describe: "The npm package name to check",
+					type: "string",
+					demandOption: true,
+				})
+				.positional("range", {
+					describe: "The semver range to check against (optional)",
+					type: "string",
+				});
+		},
+		async (argv) => {
+			try {
+				const versions = await allVersions(argv.package, argv.range);
+				const versionsWithColour = colourValidVersions(versions);
+				console.log(columns(versionsWithColour, { sort: false }));
+			} catch (error) {
+				console.error(error instanceof Error ? error.message : error);
+			}
+		},
+	)
+	.example("whatver lodash", "List all versions of lodash")
+	.example('whatver lodash "^1.1"', "List versions of lodash matching ^1.1")
+	.help()
+	.alias("help", "h")
+	.version()
+	.alias("version", "v")
+	.showHelpOnFail(false)
+	.fail((msg, _err, yargs) => {
+		// We show the help on fail manually so its coloured correctly
+		console.log(yargs.help());
+		console.error(chalk.bold(`\nError: ${msg}`));
+		process.exit(1);
+	})
+	.parse();
